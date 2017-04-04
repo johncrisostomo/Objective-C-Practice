6ecbc1632171ad08d923e4b519294e9fe3ea2284
John Crisostomo <johncrisostomo@protonmail.ch>
2017-04-04 17:00:34 +0800

    finishes NSDate section

--- TimeAfterTime/TimeAfterTime/main.m
+++ TimeAfterTime/TimeAfterTime/main.m
@@ -10,7 +10,8 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSDate *now = [NSDate date];
+        // NSDate *now = [NSDate date];
+        NSDate *now = [[NSDate alloc] init];
         NSLog(@"This NSDate object lives at %p", now);
         NSLog(@"The date is %@", now);
         
