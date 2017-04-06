8c9630a81cc1bdf06a5453c25994e388b27490e4
John Crisostomo <johncrisostomo@protonmail.ch>
2017-04-06 12:57:22 +0800

    finishes memory management example

---
+++ TimesTwo/TimesTwo/main.m
@@ -0,0 +1,28 @@
+//
+//  main.m
+//  TimesTwo
+//
+//  Created by John Crisostomo on 06/04/2017.
+//  Copyright © 2017 John Crisostomo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSDate *currentTime = [NSDate date];
+        NSLog(@"currentTime's value is %p", currentTime);
+        
+        NSDate *startTime = currentTime;
+        
+        sleep(2);
+        
+        currentTime = [NSDate date];
+        NSLog(@"currentTime's value is %p", currentTime);
+        NSLog(@"The address of the original object is %p", startTime);
+        
+        currentTime = nil;
+        NSLog(@"currentTime's value is %p", currentTime);
+    }
+    return 0;
+}
